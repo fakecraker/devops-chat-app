0d3f74f7842ada2d9079ab1ebe00cf120b9d15fe
VARUN S <varunamirrthas@gamil.com>
2025-11-11 15:02:14 +0530

    added docker file and docker compose file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -8,12 +8,19 @@
 import axios from "axios";
 
 // Decide the base URL before creating axios instance
+// const baseURL =
+//   import.meta.env.MODE === "development"
+//     ? "http://localhost:5001/api"
+//     : window.location.hostname === "3.111.198.194"
+//       ? "http://3.111.198.194:5001/api"
+//       : "/api";
+
+
 const baseURL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api"
-    : window.location.hostname === "3.111.198.194"
-      ? "http://3.111.198.194:5001/api"
-      : "/api";
+  window.location.hostname === "localhost"
+    ? "http://localhost:5001/api"                // when running locally
+    : "http://3.111.198.194:5001/api";
+
 
 export const axiosInstance = axios.create({
   baseURL,
